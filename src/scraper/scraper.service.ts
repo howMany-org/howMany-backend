28cd609286cdf19870a507b71e1e8ead097eb94b
nundung <jjy809123@naver.com>
2024-08-12 14:45:52 +0900

    Feat: steam 방송 목록을 스크래핑 해오는 api 생성

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -166,4 +166,157 @@ export class ScraperService {
       await browser.close();
     }
   }
+
+  // 방송 목록 스크래핑
+  async scrapBroadcasts() {
+    const browser = await puppeteer.launch({
+      dumpio: true,
+      headless: true,
+      devtools: true,
+      args: [
+        '--no-sandbox',
+        '--disable-gpu',
+        '--user-data-dir=/tmp/chrome-profile',
+      ],
+      executablePath:
+        '/home/ubuntu/.cache/puppeteer/chrome/linux-127.0.6533.99/chrome-linux64/chrome',
+    });
+    const page = await browser.newPage();
+    try {
+      await page.goto('https://steamcommunity.com/?subsection=broadcasts');
+
+      // 컨테이너 셀렉터 동적 생성
+      const containerSelectors = [];
+      for (let row = 1; row <= 5; row++) {
+        for (let col = 2; col <= 3; col++) {
+          containerSelectors.push(
+            `#page_1_row_${row}_template_twoLarge > div:nth-child(${col})`,
+          );
+        }
+      }
+
+      // 모든 셀렉터가 로드될 때까지 기다리기
+      await Promise.all(
+        containerSelectors.map((selector) => page.waitForSelector(selector)),
+      );
+      // 방송 목록을 추출하기
+      const broadcasts = await page.evaluate((selectors) => {
+        return selectors.flatMap((selector) => {
+          const rows = document.querySelectorAll(selector);
+          return Array.from(rows).map((row) => {
+            const linkElement = row.querySelector('a');
+            const broadcastInfoElement = row.querySelector(
+              'a > div.apphub_CardMetaData',
+            );
+            const friendBlockContainer = row.querySelector(
+              'div.apphub_friend_block_container',
+            );
+            const additionalInfo = friendBlockContainer
+              ? friendBlockContainer.querySelector()
+              : null;
+            const additionalInfoLink = friendBlockContainer
+              ? friendBlockContainer.querySelector('div > div > a:nth-child(2)')
+              : null;
+            return {
+              link: linkElement ? linkElement.href : '',
+              broadcastInfo: broadcastInfoElement
+                ? broadcastInfoElement.textContent.trim()
+                : '',
+              additionalInfo: additionalInfo,
+              additionalInfoLink: additionalInfoLink ? additionalInfo.href : '',
+            };
+          });
+        });
+      }, containerSelectors);
+
+      console.log(broadcasts);
+      return broadcasts;
+    } catch (error) {
+      console.error('Error during scraping:', error);
+    } finally {
+      await browser.close();
+    }
+  }
 }
+
+// scrapBroadcasts();
+
+//       '#page_1_row_1_template_twoLarge > div:nth-child(2) > div'
+//       '#page_1_row_1_template_twoLarge > div:nth-child(2) > a';
+// for(containerSelectors)
+//
+//       const broadcasts = await page.evaluate((selector) => {
+//         const rows = document.querySelectorAll(selector);
+//         return Array.from(rows).map((row) => {
+//           const columns = row.querySelectorAll('div:nth-child(2)');
+
+//           // 이미지 URL 추출
+//           let imageUrl = '';
+//           const imgElement = columns[2].querySelector(
+//             'a > img',
+//           ) // 이미지가 있는 <a> 내부의 <img> 태그를 찾음
+//           if (imgElement) {
+//             imageUrl = imgElement.src;
+//           }
+
+//       const broadcasts = await page.evaluate((selector) => {
+//         const element = document.querySelector(selector);
+//         return element ? element.innerHTML : null; // 요소가 존재하면 HTML을 반환
+//       }, selector);
+//       // 캐시에 데이터 저장 (TTL 15분)
+//       await this.cacheService.set(
+//         'steamStreamings',
+//         JSON.stringify(broadcasts),
+//         900,
+//       );
+//       this.logger.debug('Steam streamings data cached');
+//       return broadcasts;
+//     })});
+//      catch (error) {
+//       this.logger.error('Error during scraping:', error);
+//     } finally {
+//       await browser.close();
+//     }
+//   }
+// }
+
+//   selectors.forEach((selector) => {
+//     const container = document.querySelector(selector);
+//     if (container) {
+//       // 방송 카드 추출
+//       const items = container.querySelectorAll('.Broadcast_Card');
+//       items.forEach((item) => {
+//         const titleElement = item.querySelector(
+//           '.apphub_CardContentType',
+//         );
+//         const viewersElement = item.querySelector(
+//           '.apphub_CardContentViewers',
+//         );
+//         const imageElement = item.querySelector(
+//           '.apphub_CardContentPreviewImage',
+//         );
+//         const linkElement = item.querySelector('a');
+//         const authorElement = item.querySelector(
+//           '.apphub_CardContentAuthorName',
+//         );
+//         const authorImageElement = item.querySelector(
+//           '.appHubIconHolder img',
+//         );
+//         return {
+//           result: {
+//             title: titleElement ? titleElement.innerText.trim() : '',
+//             viewers: viewersElement
+//               ? viewersElement.innerText.trim()
+//               : '',
+//             imageUrl: imageElement ? imageElement.src : '',
+//             link: linkElement ? linkElement.href : '',
+//             author: authorElement ? authorElement.innerText.trim() : '',
+//             authorImageUrl: authorImageElement
+//               ? authorImageElement.src
+//               : '',
+//           },
+//         };
+//       });
+//     }
+//   });
+// }, containerSelectors);
