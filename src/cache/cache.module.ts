edd44164f90acd049d9e939b181d0ce7093753a1
nundung <jjy809123@naver.com>
2024-08-12 14:37:55 +0900

    Fix: 캐시메모리ttl, max 설정 수정

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,16 +1,17 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Global, Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
+import { CacheController } from './cache.controller';
 
 @Global()
 @Module({
   imports: [
     CacheModule.register({
       store: 'memory', //기본 메모리 캐시 스토어
-      max: 1000, //최대 캐시 항목 수
-      ttl: 900, //기본 TTL (초)
+      ttl: 1500, //기본 TTL (초)
     }),
   ],
+  controllers: [CacheController],
   providers: [CacheService],
   exports: [CacheService],
 })
